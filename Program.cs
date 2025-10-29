8e7c7284415370857deee4bb244f88459b8593be
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2025-10-29 07:54:13 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//*****************************************************************************
+//** 3370. Smallest Number With All Set Bits                        leetcode **
+//*****************************************************************************
+//** A number with bits all shining bright,                                  **
+//** We seek the next that’s pure with light.                                **
+//** From n we climb to find that flair                                      **
+//** All ones in binary, beyond compare.                                     **
+//*****************************************************************************
+
+int smallestNumber(int n) {
+    int powerOfLove = 1;
+    while(powerOfLove - 1 < n)
+    {
+        powerOfLove <<= 1;
+    }
+    return powerOfLove - 1;
+}
